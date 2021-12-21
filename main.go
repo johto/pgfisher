d2034987169b6b115fe1187b55d8815e9bba96b0
Marko Tiikkaja <marko@trustly.com>
2021-12-21 13:04:37 +0200

    Change command line interface to allow for different commands

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	bolt "go.etcd.io/bbolt"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -21,13 +23,38 @@ var (
 	fakeFileDest   = strings.NewReplacer("%Y", "2006", "%m", "01", "%d", "02", "%H", "15", "%M", "04", "%S", "05").Replace(logGlob)
 )
 
-func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "usage: %s DB_PATH LOG_PATH\n", os.Args[0])
+func printUsage(w io.Writer) {
+	programName := filepath.Base(os.Args[0])
+    fmt.Fprintf(w, `Usage:
+  %[1]s [--help] COMMAND [ARG]...
+
+Commands:
+
+  tail                  tails the log stream
+  initdb                initializes a database file
+
+Options:
+  --help                display this help and exit
+
+See "%[1]s COMMAND --help" for usage of a specific command.
+`, programName)
+
+}
+
+func printTailUsage(w io.Writer) {
+	programName := filepath.Base(os.Args[0])
+	fmt.Fprintf(w, `Usage:
+	%[1]s tail DB_PATH LOG_PATH
+`, programName)
+}
+
+func commandTail(args []string) {
+	if len(args) != 2 {
+		printTailUsage(os.Stderr)
 		os.Exit(1)
 	}
-	dbPath := os.Args[1]
-	logPath = os.Args[2]
+	dbPath := args[0]
+	logPath = args[1]
 
 	dbh, err := bolt.Open(dbPath, 0644, &bolt.Options{Timeout: time.Second})
 	if err != nil {
@@ -35,4 +62,39 @@ func main() {
 	}
 	pgf := NewPGFisher(dbh, ":9488")
 	pgf.MainLoop()
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage(os.Stderr)
+		os.Exit(1)
+	}
+	command := os.Args[1]
+	if command == "-h" || command == "--help" {
+		printUsage(os.Stdout)
+		os.Exit(0)
+	}
+	commandArgs := []string{}
+	if len(os.Args) > 2 {
+		commandArgs = os.Args[2:]
+	}
+
+	var printCommandUsage func (w io.Writer)
+	var executeCommand func (args []string)
+	switch command {
+		case "tail":
+			printCommandUsage = printTailUsage
+			executeCommand = commandTail
+		default:
+			fmt.Fprintf(os.Stderr, "unknown command %s\n", command)
+			os.Exit(1)
+	}
+
+	for _, arg := range commandArgs {
+		if arg == "-h" || arg == "--help" {
+			printCommandUsage(os.Stdout)
+			os.Exit(0)
+		}
+	}
+	executeCommand(commandArgs)
 }
